aa302e873c8018342a7a488bca11a9d8bfb1405c
hoshimado <xingyanhuan@yahoo.co.jp>
2024-05-29 23:30:28 +0900

    #4 静的テストのサンプルコードの説明を追加、途中まで

--- to-do-timer/src/components/TimerPanel.vue
+++ to-do-timer/src/components/TimerPanel.vue
@@ -19,15 +19,35 @@ function _secondsToMMSS(seconds) { // ToDo: utilsフォルダー配下に移動�
     return `${formattedMinutes}:${formattedSeconds}`;
 }
 const updateValue = function () {
-    remainSecText.value = _secondsToMMSS(0);
-    totalSecText.value = _secondsToMMSS(1500);
-    titleText.value = '未実装';
-    statusText.value = 'Not Working';
+    const data = props.manageToDoTimerData.getTimerData();
+    remainSecText.value = _secondsToMMSS(data.remainSec);
+    totalSecText.value = _secondsToMMSS(data.totalSec);
+    titleText.value = data.titleText;
+    statusText.value = data.statusText;
+
+    // ToDo: タイムアップ・トリガーが為されたら、音を鳴らした。。
+    // https://zenn.dev/r9uk0/articles/aabba05e827c53
+
+    setTimeout(() => {
+        updateValue();
+    }, 1000);
 }
 
 onMounted(()=>{
-    updateValue();
+    props.manageToDoTimerData.setTimerData().setTimerMaxSeconds(25*60);
+    pomodoroTimerStartNew();
+
+    setTimeout(() => {
+        updateValue();
+    }, 1);
 });
+
+const pomodoroTimerStartNew = function () {
+    props.manageToDoTimerData.setTimerData().startNew();
+}
+const pomodoroTimerStop = function () {
+    props.manageToDoTimerData.setTimerData().stop();
+}
 </script>
 
 
@@ -46,12 +66,12 @@ onMounted(()=>{
     <div class="timer-button-container">
         <div class="timer-button-group">
             <div>
-                <button type="button" class="btn btn-primary" disabled>開始</button>
+                <button type="button" class="btn btn-primary" v-on:click="pomodoroTimerStartNew">開始</button>
             </div>
         </div>
         <div class="timer-button-group">
             <div>
-                <button type="button" class="btn btn-primary" disabled>終了</button>
+                <button type="button" class="btn btn-primary" v-on:click="pomodoroTimerStop">終了</button>
             </div>
         </div>
     </div>
